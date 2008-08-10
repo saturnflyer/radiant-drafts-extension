1d675db267c5917e153bcc751c4b6c83f574a917
Jim Gay <jim@saturnflyer.com>
2008-08-10 12:52:33 -0400

    updated additions, not working yet...

--- app/models/page_part_additions_for_drafts.rb
+++ app/models/page_part_additions_for_drafts.rb
@@ -3,13 +3,22 @@ module PagePartAdditionsForDrafts
     base.class_eval {
       has_one :page_part_draft
       
-      def has_draft?
-        page_part_draft?
-      end
-      
       alias_method :draft, :page_part_draft
+      
+      include InstanceMethods
     }
   end
   module InstanceMethods
+    
+    def draft=(details)
+      puts details
+      unless draft.nil?
+        draft.update_attribute('content',details)
+      else
+        @draft = PagePartDraft.new(:content => 'details', :page_part_id => self.id, :filter_id => self.filter_id)
+        draft = @draft
+        draft.save!
+      end
+    end
   end
 end
